53a5bc681522287bd5f6e6c34e8a679e0941f1f0
Priyansh Srivastava <prisriorg@gmail.com>
2025-01-14 00:29:52 +0530

    new

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          '2xl': '1248px',
+        },
+      }
+    },
+  },
+  daisyui: {
+    themes: ["light", "dark"],
+    darkTheme: "dark",
+    logs: false,
+  },
+  plugins: [require("daisyui")],
+};
