84071fd964473ba8f9fbfac7c08cc1ca94fccad6
Priyansh Srivastava <prisriorg@gmail.com>
2025-01-14 00:58:17 +0530

    Update site URL and clean up 404 error page layout

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ export const SITE = {
   title: "PriSri Blog",
   description: "Welcome to PriSri Blog, where you can read about the latest news and articles on technology, programming, lifestyle, productivity, health, and finance.",
   author: "Mohammad Rahmani",
-  url: "https://astro-news-six.vercel.app",
+  url: "https://www.prisri.org",
   github: "https://github.com/Mrahmani71/astro-news",
   locale: "en-US",
   dir: "ltr",
