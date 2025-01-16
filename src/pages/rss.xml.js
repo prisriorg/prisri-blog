80b013807ac850c7ff9c91885066f7a474bdfe01
Priyansh Srivastava <prisriorg@gmail.com>
2025-01-16 20:53:13 +0530

    Refactor RSS feed generation: replace static blog data with dynamic article collection retrieval

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,48 +1,34 @@
 import rss from "@astrojs/rss";
 import { SITE } from "../config";
+import { getCollection } from "astro:content";
 
-export function GET(context) {
-  const blog = [
-    {
-      id: "hello-world",
-      data: {
-        title: "Hello, World!",
-        pubDate: "2022-01-01",
-        description: "Welcome to my new blog!",
-        categories: ["stars", "space"],
-      },
-    },
-    {
-      id: "hello-astro",
-      data: {
-        title: "Hello, Astro!",
-        pubDate: "2022-01-02",
-        description: "Astro is awesome!",
-        categories: ["stars", "space"],
-      },
-    },
-  ];
-
+export async function GET(context) {
+  const articles = await getCollection("articles");
   return rss({
-    // `<title>` field in output xml
     title: SITE.title,
-    // `<description>` field in output xml
     description: SITE.description,
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#site
     site: context.site,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      // Compute RSS link from post `id`
-      // This example assumes all posts are rendered as `/blog/[id]` routes
-      link: `/blog/${post.id}/`,
-      categories: post.data.categories,
+    items: articles.map((article) => ({
+      title: article.data.title,
+      pubDate: article.data.publishedTime,
+      description: article.data.description,
+      link: `/blog/${article.id}/`,
     })),
-    // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
   });
 }
+
+// export function GET(context) {
+//   return rss({
+//     items: blog.map((post) => ({
+//       title: post.data.title,
+//       pubDate: post.data.pubDate,
+//       description: post.data.description,
+//       // Compute RSS link from post `id`
+//       // This example assumes all posts are rendered as `/blog/[id]` routes
+//       link: `/blog/${post.id}/`,
+//       categories: post.data.categories,
+//     })),
+//     // (optional) inject custom xml
+//     customData: `<language>en-us</language>`,
+//   });
+// }
