461a412b046746f1cf9af2de2f97ade2fd13134e
Priyansh Srivastava <prisriorg@gmail.com>
2025-01-30 23:48:11 +0530

    Add additional redirect links to Google API route

--- src/pages/google/index.ts
+++ src/pages/google/index.ts
@@ -5,6 +5,19 @@ export const GET: APIRoute = (context) => {
   const { redirect } = context;
   const links = [
     "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://prisri.org/&ved=2ahUKEwitpZ3u-Z2LAxWASWwGHSd4HVYQFnoECAYQAQ&sqi=2&usg=AOvVaw2kPSvG2eHQdpQAB7v7geyk",
+    "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://prisri.org/blog/china-demand-for-russian-specialists-has-increased-sixfold/&ved=2ahUKEwjvwb2thp6LAxVGxzgGHXTgI1A4HhAWegQIGhAB&usg=AOvVaw3Uueu6ZxXbFbNDqdd6owco",
+    "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://prisri.org/blog/how-to-protect-money-from-devaluation-and-default/&ved=2ahUKEwjj_8PChp6LAxX9zDgGHYWgDJY4KBAWegQIHRAB&usg=AOvVaw0Z0UVme7ddPDX7yHDQK6fn",
+
+
+
+
+
+
+
+
+
+
+    
   ];
   const link = links[Math.floor(Math.random() * links.length)];
   return redirect(link, 307);
