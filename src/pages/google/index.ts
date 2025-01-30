83014b732868d0fcc21d1e49010a2a6713a8b71a
Priyansh Srivastava <prisriorg@gmail.com>
2025-01-30 22:53:20 +0530

    Remove unused index.astro file and add Google redirect API route

---
+++ src/pages/google/index.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = (context) => {
+  const { redirect } = context;
+  const links = [
+    "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://prisri.org/&ved=2ahUKEwitpZ3u-Z2LAxWASWwGHSd4HVYQFnoECAYQAQ&sqi=2&usg=AOvVaw2kPSvG2eHQdpQAB7v7geyk",
+  ];
+  const link = links[Math.floor(Math.random() * links.length)];
+  return redirect(link);
+};
